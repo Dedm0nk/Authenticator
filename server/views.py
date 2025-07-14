2b642853b2a2e77e718c36a961f86bad9b35e25a
Dedm0nk <ashinsajir@gmail.com>
2025-07-14 21:48:12 +0530

    Resetting DB

--- server/views.py
+++ server/views.py
@@ -15,7 +15,20 @@ def signup(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        user = User.objects.get(username=request.data['username'])
+        user = User.objects.get(full_name = request.data['full_name'])
+        if year_of_study := request.data.get('year_of_study'):
+            user.year_of_study = year_of_study
+        else:
+            user.year_of_study = 'Not specified'
+        if department := request.data.get('department'):
+            user.department = department
+        else:
+            user.department = 'Not specified'
+        user.email = request.data.get('email')
+        if phone_number := request.data.get('phone_number'):
+            user.phone_number = phone_number
+        else:
+            user.phone_number = 'N/A'
         user.set_password(request.data['password'])
         user.save()
         token = Token.objects.create(user=user)
@@ -24,7 +37,7 @@ def signup(request):
 
 @api_view(['POST'])
 def login(request):
-    user = get_object_or_404(User, username=request.data['username'])
+    user = get_object_or_404(User, email=request.data['email'])
     if not user.check_password(request.data['password']):
         return Response("missing user", status=status.HTTP_404_NOT_FOUND)
     token, created = Token.objects.get_or_create(user=user)
